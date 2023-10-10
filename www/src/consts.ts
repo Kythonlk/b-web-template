85434473b3beda07d3659a7b118ed8f2e1285c72
Kythonlk <kythonlk@gmail.com>
2023-10-10 21:14:16 +0530

    ewrtgewgt

--- www/src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Photography Studio';
-export const SITE_DESCRIPTION = 'We are passionate and creative photographers';
